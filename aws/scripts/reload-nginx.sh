ab607dfc38e7c788d22a17ce249d263415f7c25e
mfdsix <mfdsix.1nd0@gmail.com>
2025-12-23 14:50:22 +0700

    move config

--- aws/scripts/reload-nginx.sh
+++ aws/scripts/reload-nginx.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 systemctl reload nginx
 echo "Nginx reloaded with new build."
-
-sleep 60
-
-systemctl stop nginx
-echo "Nginx stopped after 1 minute."
---
+++ aws/scripts/stop-nginx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 60
+
+systemctl stop nginx
+echo "Nginx stopped after 1 minute."
